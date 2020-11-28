8b6dfa2442f89ec6934dc525ac59090acb7d3db0
iforgotmysocks <iforgotmysocks@users.noreply.github.com>
2020-11-28 20:59:31 +0100

    added various sizes to bounty military assistance drops

--- Source/CaravanAdventures/CaravanAdventures/CaravanMechBounty/BountyRequest.cs
+++ Source/CaravanAdventures/CaravanAdventures/CaravanMechBounty/BountyRequest.cs
@@ -15,6 +15,8 @@ namespace CaravanAdventures.CaravanMechBounty
         private Faction faction;
         private DiaNode root;
 
+        private int creditsSpent = 0;
+
         public BountyRequest(DiaNode result, Pawn negotiator, Faction faction)
         {
             this.root = result;
@@ -25,7 +27,8 @@ namespace CaravanAdventures.CaravanMechBounty
         public DiaOption CreateInitialDiaMenu()
         {
             var bountyNode = new DiaNode("CABountyExchangeMain".Translate(CompCache.StoryWC.BountyPoints));
-            bountyNode.options.Add(new DiaOption("CABountyExchangeRequestHelp".Translate()) { action = () => { SelectTargetForDrop(requestor); bountyNode.text = "CABountyExchangeMain".Translate(CompCache.StoryWC.BountyPoints); }, resolveTree = true });
+            //bountyNode.options.Add(new DiaOption("CABountyExchangeRequestHelp".Translate()) { action = () => { SelectTargetForDrop(requestor); bountyNode.text = "CABountyExchangeMain".Translate(CompCache.StoryWC.BountyPoints); }, resolveTree = true });
+            bountyNode.options.Add(new DiaOption("CABountyExchangeRequestHelp".Translate()) { link = GetDropAssistanceStrengthVariaties(bountyNode) });
             bountyNode.options.Add(new DiaOption("CABountyExchangeRequestItem".Translate()) { action = () => CompCache.StoryWC.BountyPoints += 100, link = bountyNode });
             bountyNode.options.Add(new DiaOption("CABountyExchangeRequestImprovedRelations".Translate()) { action = () => CompCache.StoryWC.BountyPoints += 100, link = bountyNode });
             bountyNode.options.Add(new DiaOption("CABountyBack".Translate()) { link = root });
@@ -33,11 +36,42 @@ namespace CaravanAdventures.CaravanMechBounty
             return new DiaOption("CABountyExchangeOpenOption".Translate()) { link = bountyNode };
         }
 
-        private void SelectTargetForDrop(Pawn negotiator)
+        private DiaNode GetDropAssistanceStrengthVariaties(DiaNode parent)
         {
-            CameraJumper.TryJump(CameraJumper.GetWorldTarget(negotiator.Map.Parent));
+            var bountyNode = new DiaNode("CABountyExchangeRequestHelp_StrengthSelection".Translate(CompCache.StoryWC.BountyPoints));
+            //bountyNode.options.Add(new DiaOption("CABountyExchangeRequestHelp".Translate()) { action = () => { SelectTargetForDrop(requestor); bountyNode.text = "CABountyExchangeMain".Translate(CompCache.StoryWC.BountyPoints); }, resolveTree = true });
+            bountyNode.options.Add(HunterDropRequest(250, "CABountyExchangeRequestHelp_StrengthSelection_Few"));
+            bountyNode.options.Add(HunterDropRequest(650, "CABountyExchangeRequestHelp_StrengthSelection_Bunch"));
+            bountyNode.options.Add(HunterDropRequest(1250, "CABountyExchangeRequestHelp_StrengthSelection_Army"));
+            bountyNode.options.Add(new DiaOption("CABountyBack".Translate()) { link = parent });
+
+            return bountyNode;
+        }
+
+        private DiaOption HunterDropRequest(int credit, string label)
+        {
+            var option = new DiaOption(label.Translate());
+            if (CompCache.StoryWC.BountyPoints < credit)
+            {
+                option.disabled = true;
+                option.disabledReason = "CABountyExchangeRequestHelp_StrengthSelection_NotEnoughMoney".Translate();
+            }
+
+            if (option.disabled) return option;
+            option.resolveTree = true;
+            option.action = () => {
+                
+                creditsSpent = credit;
+                SelectTargetForDrop();
+            };
+            return option;
+        }
+
+        private void SelectTargetForDrop()
+        {
+            CameraJumper.TryJump(CameraJumper.GetWorldTarget(requestor.Map.Parent));
             Find.WorldSelector.ClearSelection();
-            int tile = negotiator.Map.Tile;
+            int tile = requestor.Map.Tile;
             Find.WorldTargeter.BeginTargeting_NewTemp(ChoseWorldTarget, true, CompLaunchable.TargeterMouseAttachment, true, delegate
             {
                 //GenDraw.DrawWorldRadiusRing(tile, this.MaxLaunchDistance);
@@ -49,7 +83,13 @@ namespace CaravanAdventures.CaravanMechBounty
             if (!target.IsValid) return false;
             var map = Find.Maps.FirstOrDefault(t => t.Tile == target.Tile && t.mapPawns.AnyColonistSpawned);
             if (map == null) return false;
-            CaravanStory.StoryUtility.GetAssistanceFromAlliedFaction(faction, map, 3000, 3500);
+            if (creditsSpent == 0)
+            {
+                Log.Warning("Calculation of assistance points for bounty failed, selecting lowest option");
+                creditsSpent = 260;
+            }
+            CompCache.StoryWC.BountyPoints -= creditsSpent;
+            CaravanStory.StoryUtility.GetAssistanceFromAlliedFaction(faction, map, creditsSpent * 2, creditsSpent * 2);
             return true;
         }
 
