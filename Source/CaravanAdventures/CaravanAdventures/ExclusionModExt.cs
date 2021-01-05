8a834ebcda01d5fde2a3adad5ae6a6cfa6dacc04
iforgotmysocks <iforgotmysocks@users.noreply.github.com>
2021-01-05 02:10:41 +0100

    added exclusion mod ext

---
+++ Source/CaravanAdventures/CaravanAdventures/ExclusionModExt.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace CaravanAdventures
+{
+    class ExclusionModExt : DefModExtension
+    {
+        // todo implement
+        public bool excludeFromRelations;
+        // todo create external mod ext for bounty mechs!
+        public bool excludeMechFromBounty;
+    }
+}
