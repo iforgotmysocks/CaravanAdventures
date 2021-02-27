2ea578bf6123261b85a6959fa3099b59709aba95
iforgotmysocks <iforgotmysocks@users.noreply.github.com>
2021-02-28 00:56:57 +0100

    added circling blade attack

---
+++ Source/CaravanAdventures/CaravanAdventures/CaravanStory/MechChips/AbilityMotes/CirclingBlades.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace CaravanAdventures.CaravanStory.MechChips.AbilityMotes
+{
+    class CirclingBlades : Mote
+    {
+        public LocalTargetInfo launchObject;
+
+        public override void Draw()
+        {
+            base.Draw();
+
+            //    Vector3 size = new Vector3(3, 1f, 3);
+            //    Matrix4x4 matrix = default(Matrix4x4);
+            //    Vector3 pos = pawnDrawPos;
+            //    matrix.SetTRS(pos, Quaternion.AngleAxis(angle, Vector3.up), size);
+            //    Graphics.DrawMesh(MeshPool.plane10, matrix, matBlade, 0);
+        }
+
+        protected override void TimeInterval(float deltaTime)
+        {
+            DLog.Message($"deltatime: {deltaTime} rotation: {this.exactRotation} pos: {this.exactPosition}");
+            base.TimeInterval(deltaTime);
+            this.exactRotation += this.rotationRate * deltaTime;
+            this.exactPosition = launchObject.Thing.DrawPos;
+        }
+
+    }
+}
--- Source/CaravanAdventures/CaravanAdventures/CaravanStory/MechChips/HediffComp_EXT1Melee.cs
+++ Source/CaravanAdventures/CaravanAdventures/CaravanStory/MechChips/HediffComp_EXT1Melee.cs
@@ -16,6 +16,7 @@ namespace CaravanAdventures.CaravanStory.MechChips
     {
         private int ticks = 0;
         private List<Pawn> producedMechs;
+        private AbilityMotes.CirclingBlades blades;
 
         public HediffCompProperties_EXT1Melee Props => (HediffCompProperties_EXT1Melee)props;
 
@@ -35,19 +36,34 @@ namespace CaravanAdventures.CaravanStory.MechChips
         public override void CompPostTick(ref float severityAdjustment)
         {
             base.CompPostTick(ref severityAdjustment);
-            if (Pawn?.Destroyed != false || !Pawn.Awake()) return;
+            if (Pawn?.Destroyed != false || !Pawn.Awake() || Pawn.Map == null) return;
             
-            if (ticks % 300 == 0)
+            if (ticks % 25 == 0 && blades != null && blades.Spawned)
             {
-                SliceEnemiesInfront();
+                SliceSurroundingEnemies();
+            }
+
+            if (ticks % 600 == 0)
+            {
+                if (GenRadial.RadialCellsAround(Pawn.Position, 2, false).Select(x => x.GetFirstPawn(Pawn.Map)).Count() != 0)
+                {
+                    blades = (AbilityMotes.CirclingBlades)ThingMaker.MakeThing(ThingDef.Named("CACirclingBlades"));
+                    blades.Attach(Pawn);
+                    blades.launchObject = Pawn;
+                    blades.rotationRate = 1200;
+                    blades.Scale = 2.5f;
+                    blades.exactPosition = Pawn.DrawPos;
+                    blades.solidTimeOverride = -1f;
+                    GenSpawn.Spawn(blades, Pawn.Position, Pawn.Map);
+                }
             }
 
             if (ticks % 1250 == 0)
             {
-                SliceSurroundingEnemies();
+                //SliceSurroundingEnemies();
             }
             
-            if (ticks >= 2500)
+            if (ticks >= 1300)
             {
                 if (JumpToTarget()) ticks = 0;
             }
@@ -69,42 +85,56 @@ namespace CaravanAdventures.CaravanStory.MechChips
             }
         }
 
+        
+
+        
         protected bool JumpToTarget()
         {
             // todo get all targets in the desired range that are in sight, calculate the one with the most bystanding enemies
-            if (Pawn?.TargetCurrentlyAimingAt != null && Pawn.TargetCurrentlyAimingAt != LocalTargetInfo.Invalid && Pawn.TargetCurrentlyAimingAt.Cell.DistanceTo(Pawn.Position) >= 10f)
+            if (Pawn?.TargetCurrentlyAimingAt != null && Pawn.TargetCurrentlyAimingAt != LocalTargetInfo.Invalid && Pawn.TargetCurrentlyAimingAt.Cell.DistanceTo(Pawn.Position) >= 4f)
             {
-                if (!GenGrid.InBounds(Pawn.TargetCurrentlyAimingAt.Cell, Pawn.Map))
+
+
+                PawnFlyer pawnFlyer = PawnFlyer.MakeFlyer(ThingDefOf.PawnJumper, Pawn, Pawn.Position);
+                if (pawnFlyer != null)
                 {
-                    DLog.Message($"Target cell out of bounds!");
-                    return false;
+                    GenSpawn.Spawn(pawnFlyer, Pawn.TargetCurrentlyAimingAt.Cell, Pawn.Map, WipeMode.Vanish);
+                    return true;
                 }
-                StoryUtility.CallBombardment(Pawn.TargetCurrentlyAimingAt.Cell, Pawn.Map, Pawn);
+
+
+                //if (!GenGrid.InBounds(Pawn.TargetCurrentlyAimingAt.Cell, Pawn.Map))
+                //{
+                //    DLog.Message($"Target cell out of bounds!");
+                //    return false;
+                //}
+                //StoryUtility.CallBombardment(Pawn.TargetCurrentlyAimingAt.Cell, Pawn.Map, Pawn);
                 return true;
             }
+
+          
+
             return false;
         }
 
         protected void SliceSurroundingEnemies()
         {
-            var cells = GenRadial.RadialCellsAround(Pawn.Position, 6, false).Where(cell => cell.Standable(Pawn.Map));
+            var cells = GenRadial.RadialCellsAround(Pawn.Position, 2, false).Where(cell => cell.Standable(Pawn.Map));
             var pawns = cells.SelectMany(cell => cell.GetThingList(Pawn.Map).OfType<Pawn>().Where(pawn => !pawn.RaceProps.IsMechanoid)).ToList();
             if (pawns != null && pawns.Count() != 0)
             {
                 foreach (var cell in cells)
                 {
-                    //MoteMaker.MakeStaticMote(cell, Pawn.Map, ThingDef.Named("Mote_LongSparkThrown"));
-                    MoteMaker.MakeStaticMote(cell, Pawn.Map, ThingDef.Named("Mote_BlastFlame"));
-                    //MoteMaker.MakeStaticMote(cell, Pawn.Map, ThingDef.Named("Mote_DustSlow"));
-                    MoteMaker.ThrowDustPuffThick(cell.ToVector3(), Pawn.Map, Rand.Range(1.5f, 3f), new Color(1f, 1f, 1f, 2.5f));
-                    MoteMaker.ThrowAirPuffUp(cell.ToVector3(), Pawn.Map);
+                    //    MoteMaker.MakeStaticMote(cell, Pawn.Map, ThingDef.Named("Mote_BlastFlame"));
+                    //    MoteMaker.ThrowDustPuffThick(cell.ToVector3(), Pawn.Map, Rand.Range(1.5f, 3f), new Color(1f, 1f, 1f, 2.5f));
+                    //    MoteMaker.ThrowAirPuffUp(cell.ToVector3(), Pawn.Map);
 
                     //var cellPawns = cell.GetThingList(Pawn.Map).OfType<Pawn>().ToList();
                     //cellPawns.ForEach(pawn => pawn.TakeDamage(new DamageInfo(DamageDefOf.Burn, 20, 50, -1, Pawn, pawn.health.hediffSet.GetRandomNotMissingPart(DamageDefOf.Burn, BodyPartHeight.Undefined, BodyPartDepth.Outside))));
                 }
-                pawns.ForEach(pawn => Enumerable.Range(0, 3).ToList().ForEach(run => pawn.TakeDamage(new DamageInfo(DamageDefOf.Flame, 20, 0.5f, -1, Pawn, pawn.health.hediffSet.GetRandomNotMissingPart(DamageDefOf.Burn, BodyPartHeight.Undefined, BodyPartDepth.Outside)))));
-
-                SoundDef.Named("DropPod_Leaving").PlayOneShot(new TargetInfo(Pawn.Position, Pawn.Map, false));
+                pawns.ForEach(pawn => Enumerable.Range(0, 3).ToList().ForEach(run => pawn.TakeDamage(new DamageInfo(DamageDefOf.Scratch, 20, 0.5f, -1, Pawn, pawn.health.hediffSet.GetRandomNotMissingPart(DamageDefOf.Burn, BodyPartHeight.Undefined, BodyPartDepth.Outside)))));
+                SoundDefOf.MetalHitImportant.PlayOneShot(new TargetInfo(Pawn.Position, Pawn.Map, false));
+                //SoundDef.Named("DropPod_Leaving").PlayOneShot(new TargetInfo(Pawn.Position, Pawn.Map, false));
             }
         }
 
