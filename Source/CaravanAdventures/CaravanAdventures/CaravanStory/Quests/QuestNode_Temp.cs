5714f87752a2475a401b92cf6062df9fb19bbe6b
iforgotmysocks <iforgotmysocks@users.noreply.github.com>
2021-05-23 03:14:08 +0200

    cleanup

--- Source/CaravanAdventures/CaravanAdventures/CaravanStory/Quests/QuestNode_Temp.cs
+++ Source/CaravanAdventures/CaravanAdventures/CaravanStory/Quests/QuestNode_Temp.cs
@@ -12,25 +12,10 @@ namespace CaravanAdventures.CaravanStory.Quests
         protected override void RunInt()
         {
             var quest = QuestGen.quest;
-            //var slate = QuestGen.slate;
             if (!StartQuest()) return;
-
-            //var rules = new List<Rule>();
-            //Log.Message($"Storycontact name: {CompCache.StoryWC.questCont.Village.StoryContact.Name}");
-            //rules.Add(new Rule_String("faction_name", CompCache.StoryWC.questCont.Village.StoryContact.Faction.Name.ToString()));
-            //rules.Add(new Rule_String("settlement_name", CompCache.StoryWC.questCont.Village.Settlement.Name.Colorize(UnityEngine.Color.cyan)));
-            //rules.AddRange(GrammarUtility.RulesForPawn("pawn", CompCache.StoryWC.questCont.Village.StoryContact, null, true, true));
-            //QuestGen.AddQuestDescriptionRules(rules);
         }
 
-        private bool StartQuest()
-        {
-            return true;
-        }
-
-        protected override bool TestRunInt(Slate slate)
-        {
-            return true;
-        }
+        private bool StartQuest() => true;
+        protected override bool TestRunInt(Slate slate) => true;
     }
 }
