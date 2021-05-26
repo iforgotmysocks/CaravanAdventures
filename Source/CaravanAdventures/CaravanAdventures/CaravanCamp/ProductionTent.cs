c5925703f645b3a161ebfb63abf8da14d53bb3f7
iforgotmysocks <iforgotmysocks@users.noreply.github.com>
2021-05-26 11:13:33 +0200

    fix

--- Source/CaravanAdventures/CaravanAdventures/CaravanCamp/ProductionTent.cs
+++ Source/CaravanAdventures/CaravanAdventures/CaravanCamp/ProductionTent.cs
@@ -116,7 +116,7 @@ namespace CaravanAdventures.CaravanCamp
             var parkaBill = new Bill_ProductionWithUft(DefDatabase<RecipeDef>.GetNamed("Make_Apparel_Parka")) { repeatMode = BillRepeatModeDefOf.TargetCount, targetCount = 1, hpRange = new FloatRange(0.9f, 1f), includeTainted = false, qualityRange = new QualityRange(QualityCategory.Normal, QualityCategory.Legendary) };
             handTailoringBench.BillStack.AddBill(parkaBill);
 
-            var fuelFromCorpseBill = new Bill_ProductionWithUft(DefDatabase<RecipeDef>.GetNamed("CAMake_ChemfuelFromCorpses")) { repeatMode = BillRepeatModeDefOf.Forever, ingredientFilter = new ThingFilter( };
+            var fuelFromCorpseBill = new Bill_ProductionWithUft(DefDatabase<RecipeDef>.GetNamed("CAMake_ChemfuelFromCorpses")) { repeatMode = BillRepeatModeDefOf.Forever };
             fuelFromCorpseBill.ingredientFilter.SetAllow(SpecialThingFilterDef.Named("AllowCorpsesColonist"), false);
             refinery.BillStack.AddBill(fuelFromCorpseBill);
 
