3c07df47f57da8851db30407532be9acefa13567
iforgotmysocks <iforgotmysocks@users.noreply.github.com>
2021-07-19 23:36:19 +0200

    adjusted general setting description for better shrine rewards

--- Source/CaravanAdventures/CaravanAdventures/Settings/SettingsOther.cs
+++ Source/CaravanAdventures/CaravanAdventures/Settings/SettingsOther.cs
@@ -20,7 +20,7 @@ namespace CaravanAdventures.Settings
 
             width = 600f;
         }
-
+        
         protected override void SetInitialSizeAndPosition()
         {
             windowRect = new Rect((UI.screenWidth - width) / 2f, (UI.screenHeight - InitialSize.y) / 2f, width, InitialSize.y);
@@ -38,7 +38,7 @@ namespace CaravanAdventures.Settings
 
             options.CheckboxLabeled("Auto-cleanup for abandoned settlements by the player", ref ModSettings.autoRemoveAbandondSettlementRuins, "Doesn't influence camp behaviour, camps won't leave ruins either way");
             options.CheckboxLabeled("Increase npc settlement's available silver and food by 2-3 and 1.5-2, respectively", ref ModSettings.buffSettlementFoodAndSilverAvailability);
-            options.CheckboxLabeled("Increased ancient shrine rewards chance (hover for more info)", ref ModSettings.buffShrineRewards, "Adds the chance for a second valuable item when raiding ancient shrines (heavily nerfed for story master shrines)");
+            options.CheckboxLabeled("Increased ancient shrines and reward chance (hover for more info)", ref ModSettings.buffShrineRewards, "Adds the possibility for an additional ancient danger to appear and adds a chance for a second valuable item when raiding ancient shrines (heavily nerfed for story master shrines)");
             options.CheckboxLabeled("Increase skill point decay level from 10 to 15", ref ModSettings.spDecayLevelIncrease, "Increases the level to 15, when the skill decay starts to set in, so your travelers have it easier being on the road for longer durations");
             options.CheckboxLabeled("Show letter removal icon", ref ModSettings.showLetterRemoval);
             options.End();
