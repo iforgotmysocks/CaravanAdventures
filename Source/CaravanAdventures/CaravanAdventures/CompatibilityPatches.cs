65efe540c65a37deea90b873bdf328a8441c3812
iforgotmysocks <iforgotmysocks@users.noreply.github.com>
2021-08-27 00:21:19 +0200

    added version number to patches completed message

--- Source/CaravanAdventures/CaravanAdventures/CompatibilityPatches.cs
+++ Source/CaravanAdventures/CaravanAdventures/CompatibilityPatches.cs
@@ -73,7 +73,7 @@ namespace CaravanAdventures
                 }
             });
 
-            Log.Message($"CA (1.2) patches complete.");
+            Log.Message($"CA v{System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString().TrimEnd(new[] { '.', '0' })} (1.2) patches complete.");
         }
 
         public static void ExecuteHarmonyCompatibilityPatches(Harmony harmony)
