d50ca9c33c15aad0de090d6f5f5dfc56d73c665e
iforgotmysocks <iforgotmysocks@users.noreply.github.com>
2021-09-16 11:41:22 +0200

    removed survival meals from allowed items in food tent

--- Source/CaravanAdventures/CaravanAdventures/CaravanCamp/FoodTent.cs
+++ Source/CaravanAdventures/CaravanAdventures/CaravanCamp/FoodTent.cs
@@ -42,6 +42,7 @@ namespace CaravanAdventures.CaravanCamp
             zone.settings.filter = new ThingFilter();
             zone.settings.filter.SetAllow(SpecialThingFilterDef.Named("AllowRotten"), false);
             zone.settings.filter.SetAllow(ThingCategoryDefOf.Foods, true);
+            zone.settings.filter.SetAllow(ThingDefOf.MealSurvivalPack, false);
             zone.settings.filter.SetAllow(ThingDefOf.Kibble, false);
             zone.settings.filter.SetAllow(ThingDefOf.Hay, false);
             zone.settings.filter.SetAllow(ThingDef.Named("Meat_Megaspider"), false);
