c587538b85e3255258cfbc05d52cf4d998bb4c15
iforgotmysocks <iforgotmysocks@users.noreply.github.com>
2022-01-09 02:08:30 +0100

    removed DLog ignore limit due to being obsolete

--- Source/CaravanAdventures/CaravanAdventures/DLog.cs
+++ Source/CaravanAdventures/CaravanAdventures/DLog.cs
@@ -1,28 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Verse;
+﻿using Verse;
 
 namespace CaravanAdventures
 {
     class DLog
     {
         private static bool shouldLog => ModSettings.debugMessages;
-        public static void Message(string message, bool ignoreLimit = false)
+        public static void Message(string message)
         {
-            if (shouldLog) Log.Message(message, ignoreLimit);
+            if (shouldLog) Log.Message(message);
         }
 
-        public static void Warning(string message, bool ignoreLimit = false)
+        public static void Warning(string message)
         {
-            if (shouldLog) Log.Warning(message, ignoreLimit);
+            if (shouldLog) Log.Warning(message);
         }
 
-        public static void Error (string message, bool ignoreLimit = false)
+        public static void Error (string message)
         {
-            if (shouldLog) Log.Error(message, ignoreLimit);
+            if (shouldLog) Log.Error(message);
         }
 
     }
