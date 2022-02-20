17b7e0e1534644e9e072d2d6ccb0b3cdb864dced
iforgotmysocks <iforgotmysocks@users.noreply.github.com>
2022-02-20 12:35:52 +0100

    added winston waves compatibility patch

---
+++ Source/CaravanAdventures/CaravanAdventures/Patches/Compatibility/WinstonWavesPatch.cs
@@ -0,0 +1,43 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Linq;
+using UnityEngine;
+using Verse;
+using System.Reflection;
+using System;
+
+namespace CaravanAdventures.Patches.Compatibility
+{
+    class WinstonWavesPatch
+    {
+        public static void ApplyPatches(Assembly assembly)
+        {
+            if (!ModSettings.storyEnabled) return;
+            var org = AccessTools.Method(assembly.GetType("VSEWW.IncidentWorker_Raid_TryExecuteWorker_Patch"), "Prefix");
+            var pre = new HarmonyMethod(typeof(WinstonWavesPatch).GetMethod(nameof(VSEWW_IncidentWorker_Raid_TryExecuteWorker_Patch_Prefix_Prefix)));
+            HarmonyPatcher.harmony.Patch(org, pre, null);
+
+            var org2 = AccessTools.Method(assembly.GetType("VSEWW.IncidentWorker_Raid_TryGenerateRaidInfo_Patch"), "Prefix");
+            var pre2 = new HarmonyMethod(typeof(WinstonWavesPatch).GetMethod(nameof(VSEWW_IncidentWorker_Raid_TryGenerateRaidInfo_Patch_Prefix_Prefix)));
+            HarmonyPatcher.harmony.Patch(org2, pre2, null);
+        }
+
+        public static bool VSEWW_IncidentWorker_Raid_TryExecuteWorker_Patch_Prefix_Prefix(IncidentParms parms, ref bool __result)
+        {
+            if (Find.Storyteller?.def?.defName != "VSE_WinstonWave") return true;
+            if (parms?.faction != CaravanStory.StoryUtility.FactionOfSacrilegHunters
+                || CaravanStory.StoryUtility.FactionOfSacrilegHunters.HostileTo(Faction.OfPlayer)) return true;
+            __result = true;
+            return false;
+        }
+
+        public static bool VSEWW_IncidentWorker_Raid_TryGenerateRaidInfo_Patch_Prefix_Prefix(IncidentParms parms, ref bool __result)
+        {
+            if (Find.Storyteller?.def?.defName != "VSE_WinstonWave") return true;
+            if (parms?.faction != CaravanStory.StoryUtility.FactionOfSacrilegHunters
+                || CaravanStory.StoryUtility.FactionOfSacrilegHunters.HostileTo(Faction.OfPlayer)) return true;
+            __result = true;
+            return false;
+        }
+    }
+}
