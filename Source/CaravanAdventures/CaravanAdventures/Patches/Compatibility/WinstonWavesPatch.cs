d09a804c7ac43f5c21b1a490c13caf502f6c142a
iforgotmysocks <iforgotmysocks@users.noreply.github.com>
2022-02-25 13:47:17 +0100

    cleanup

--- Source/CaravanAdventures/CaravanAdventures/Patches/Compatibility/WinstonWavesPatch.cs
+++ Source/CaravanAdventures/CaravanAdventures/Patches/Compatibility/WinstonWavesPatch.cs
@@ -14,44 +14,33 @@ namespace CaravanAdventures.Patches.Compatibility
         {
             if (!ModSettings.storyEnabled) return;
             var org = AccessTools.Method(assembly.GetType("VSEWW.IncidentWorker_Raid_TryExecuteWorker_Patch"), "Prefix");
-            var pre = new HarmonyMethod(typeof(WinstonWavesPatch).GetMethod(nameof(VSEWW_IncidentWorker_Raid_TryExecuteWorker_Patch_Prefix_Prefix)));
+            var pre = new HarmonyMethod(typeof(WinstonWavesPatch).GetMethod(nameof(VSEWW_IncidentWorker_Raid_TryExecuteWorker_And_TryGenerateRaidInfo_Prefix_Prefix)));
             HarmonyPatcher.harmony.Patch(org, pre, null);
 
             var org2 = AccessTools.Method(assembly.GetType("VSEWW.IncidentWorker_Raid_TryGenerateRaidInfo_Patch"), "Prefix");
-            var pre2 = new HarmonyMethod(typeof(WinstonWavesPatch).GetMethod(nameof(VSEWW_IncidentWorker_Raid_TryGenerateRaidInfo_Patch_Prefix_Prefix)));
+            var pre2 = new HarmonyMethod(typeof(WinstonWavesPatch).GetMethod(nameof(VSEWW_IncidentWorker_Raid_TryExecuteWorker_And_TryGenerateRaidInfo_Prefix_Prefix)));
             HarmonyPatcher.harmony.Patch(org2, pre2, null);
         }
 
-        public static bool VSEWW_IncidentWorker_Raid_TryExecuteWorker_Patch_Prefix_Prefix(IncidentParms parms, ref bool __result)
+        public static bool VSEWW_IncidentWorker_Raid_TryExecuteWorker_And_TryGenerateRaidInfo_Prefix_Prefix(IncidentParms parms, ref bool __result)
         {
-            if (Find.Storyteller?.def?.defName != "VSE_WinstonWave") return true;
-            var mapParentDef = parms?.target is Map map ? map?.Parent?.def : null;
-            if (parms?.faction == CaravanStory.StoryUtility.FactionOfSacrilegHunters
-                    && !CaravanStory.StoryUtility.FactionOfSacrilegHunters.HostileTo(Faction.OfPlayer)
-                || mapParentDef != null
-                    && (mapParentDef == CaravanStory.CaravanStorySiteDefOf.CAStoryVillageMP
-                        || mapParentDef == CaravanStory.CaravanStorySiteDefOf.CAAncientMasterShrineMP))
-            {
-                __result = true;
-                return false;
-            }
-            return true;
+            if (!CancelWWRaidInterruption(parms)) return true;
+            __result = true;
+            return false;
         }
 
-        public static bool VSEWW_IncidentWorker_Raid_TryGenerateRaidInfo_Patch_Prefix_Prefix(IncidentParms parms, ref bool __result)
+        private static bool CancelWWRaidInterruption(IncidentParms parms)
         {
-            if (Find.Storyteller?.def?.defName != "VSE_WinstonWave") return true;
+            if (Find.Storyteller?.def?.defName != "VSE_WinstonWave") return false;
             var mapParentDef = parms?.target is Map map ? map?.Parent?.def : null;
-            if (parms?.faction == CaravanStory.StoryUtility.FactionOfSacrilegHunters 
+            return parms?.faction == CaravanStory.StoryUtility.FactionOfSacrilegHunters
                     && !CaravanStory.StoryUtility.FactionOfSacrilegHunters.HostileTo(Faction.OfPlayer)
-                || mapParentDef != null 
-                    && (mapParentDef == CaravanStory.CaravanStorySiteDefOf.CAStoryVillageMP 
-                        || mapParentDef == CaravanStory.CaravanStorySiteDefOf.CAAncientMasterShrineMP))
-            {
-                __result = true;
-                return false;
-            }
-            return true;
+                || mapParentDef != null
+                    && (new[] { 
+                        CaravanStory.CaravanStorySiteDefOf.CAStoryVillageMP, 
+                        CaravanStory.CaravanStorySiteDefOf.CAAncientMasterShrineMP, 
+                        CaravanStory.CaravanStorySiteDefOf.CALastJudgmentMP })
+                        .Contains(mapParentDef); 
         }
     }
 }
