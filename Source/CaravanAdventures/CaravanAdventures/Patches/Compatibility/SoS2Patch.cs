7cd4a6b8fb891b8eed4a2834822cb2751ee38081
iforgotmysocks <iforgotmysocks@users.noreply.github.com>
2022-06-04 10:56:41 +0200

    fixed issue with cached sos2 heat absorbing pawn

--- Source/CaravanAdventures/CaravanAdventures/Patches/Compatibility/SoS2Patch.cs
+++ Source/CaravanAdventures/CaravanAdventures/Patches/Compatibility/SoS2Patch.cs
@@ -39,6 +39,11 @@ namespace CaravanAdventures.Patches.Compatibility
             }
         }
 
+        internal static void Reset()
+        {
+            capableAuraPawn = null;
+        }
+
         private static void LoadReflectionNecessities()
         {
             compShipHeatSourceType = assembly.GetType("RimWorld.CompShipHeatSource");
