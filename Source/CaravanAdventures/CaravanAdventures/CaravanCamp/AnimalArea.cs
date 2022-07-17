224904c3daba3705e6f3751318218ae9ef0319cf
iforgotmysocks <iforgotmysocks@users.noreply.github.com>
2022-07-17 04:05:46 +0200

    replaced food zone with shelf for hq camp

--- Source/CaravanAdventures/CaravanAdventures/CaravanCamp/AnimalArea.cs
+++ Source/CaravanAdventures/CaravanAdventures/CaravanCamp/AnimalArea.cs
@@ -7,11 +7,12 @@ using Verse;
 
 namespace CaravanAdventures.CaravanCamp
 {
-    public class AnimalArea : CampArea, IAreaRestrictionTent, IZoneTent
+    public class AnimalArea : CampArea, IAreaRestrictionTent, IZoneTent, IShelfTent
     {
         protected Area_Allowed animalArea;
         protected Zone_Stockpile zone;
         protected bool tribal = false;
+        protected Building_Storage shelf;
 
         public AnimalArea()
         {
@@ -29,7 +30,7 @@ namespace CaravanAdventures.CaravanCamp
             {
                 if (entranceCells.Contains(edgeCell)) continue;
                 var thing = ThingMaker.MakeThing(ThingDefOf.Fence, ThingDefOf.WoodLog);
-                thing.SetFaction(Faction.OfPlayer); 
+                thing.SetFaction(Faction.OfPlayer);
                 campAssetListRef.Add(GenSpawn.Spawn(thing, edgeCell, map));
             }
 
@@ -37,11 +38,29 @@ namespace CaravanAdventures.CaravanCamp
             for (int i = 0; i < 6; i++)
             {
                 if (i == 0 || i % 2 == 0) continue;
-                var animalPlace = CampHelper.PrepAndGenerateThing(tribal 
-                    ? ThingMaker.MakeThing(ThingDef.Named("AnimalSleepingSpot")) 
+                var animalPlace = CampHelper.PrepAndGenerateThing(tribal
+                    ? ThingMaker.MakeThing(ThingDef.Named("AnimalSleepingSpot"))
                     : ThingMaker.MakeThing(ThingDef.Named("AnimalBed"), ThingDefOf.Leather_Plain), innerCells[i], map, default, campAssetListRef) as Building_Bed;
                 if (animalPlace != null) animalPlace.Medical = true;
             }
+
+            if (!tribal)
+            {
+                var cellSpotShelf = CellRect.Cells.FirstOrDefault(cell => cell.x == CellRect.maxX - 1 && cell.z == CellRect.maxZ - 2);
+                //map.thingGrid.ThingAt(cellSpotShelf, ThingCategory.Building).Destroy();
+
+                shelf = (Building_Storage)ThingMaker.MakeThing(ThingDef.Named("Shelf"), ThingDefOf.WoodLog);
+                shelf.SetFaction(Faction.OfPlayer);
+                shelf.GetStoreSettings().filter = new ThingFilter();
+                if (!ModSettings.useAnimalOnlyFoodForAnimalArea) shelf.GetStoreSettings().filter.SetAllow(ThingCategoryDefOf.PlantFoodRaw, true);
+                shelf.GetStoreSettings().filter.SetAllow(ThingDefOf.Hay, true);
+                shelf.GetStoreSettings().filter.SetAllow(ThingDefOf.Kibble, true);
+                campAssetListRef.Add(GenSpawn.Spawn(shelf, cellSpotShelf, map, Rot4.West, WipeMode.Vanish));
+            }
+
+            //var targetCells = CellRect.Cells.Where(cell => !CellRect.EdgeCells.Contains(cell));
+            //if (!targetCells.Any(x => x.GetTerrain(map) == TerrainDefOf.Bridge) && targetCells.All(x => x.GetTerrain(map).affordances.Contains(TerrainAffordanceDefOf.Heavy)))
+            //    foreach (var cell in targetCells) map.terrainGrid.SetTerrain(cell, DefDatabase<TerrainDef>.GetNamedSilentFail("StrawMatting"));
 
             CampHelper.PrepAndGenerateThing(ThingMaker.MakeThing(ThingDef.Named("PenMarker"), ThingDefOf.WoodLog), CellRect.CenterCell, map, default, campAssetListRef);
         }
@@ -72,6 +91,7 @@ namespace CaravanAdventures.CaravanCamp
 
         public virtual void CreateZone(Map map)
         {
+            if (!tribal) return;
             var zoneCells = CellRect.Height > CellRect.Width
                 ? CellRect.Cells.Where(cell => cell.z == CellRect.maxZ - 1 && !CellRect.EdgeCells.Contains(cell))
                 : CellRect.Cells.Where(cell => cell.x == CellRect.maxX - 1 && !CellRect.EdgeCells.Contains(cell));
@@ -82,13 +102,28 @@ namespace CaravanAdventures.CaravanCamp
             if (!ModSettings.useAnimalOnlyFoodForAnimalArea) zone.settings.filter.SetAllow(ThingCategoryDefOf.PlantFoodRaw, true);
             zone.settings.filter.SetAllow(ThingDefOf.Hay, true);
             zone.settings.filter.SetAllow(ThingDefOf.Kibble, true);
-            zone.settings.Priority = StoragePriority.Preferred;
+            zone.settings.Priority = StoragePriority.Important;
             zone.label = "CAAnimalFoodZoneLabel".Translate();
             zoneCells.ToList().ForEach(cell => zone.AddCell(cell));
         }
 
+        public void FillShelfs(Map map, Caravan caravan)
+        {
+            if (shelf == null) return;
+            foreach (var cell in shelf.AllSlotCells())
+            {
+                var medicine = caravan.AllThings.FirstOrDefault(thing => (new[] {ThingDefOf.Hay, ThingDefOf.Kibble}).Contains(thing.def));
+                if (medicine == null) medicine = caravan.AllThings.FirstOrDefault(thing => thing.HasThingCategory(ThingCategoryDefOf.PlantFoodRaw));
+                if (medicine == null) break;
+                if (!cell.Filled(map)) GenDrop.TryDropSpawn(medicine, cell, map, ThingPlaceMode.Direct, out var result);
+            }
+        }
+
+        public Building_Storage GetShelf() => shelf;
+
         public virtual void ApplyInventory(Map map, Caravan caravan)
         {
+            if (!tribal || zone == null) return;
             foreach (var cell in zone.Cells)
             {
                 var stack = caravan.AllThings.Where(x => x.def == ThingDefOf.Kibble).OrderByDescending(x => x.stackCount).FirstOrDefault()
@@ -111,5 +146,6 @@ namespace CaravanAdventures.CaravanCamp
                 }
             }
         }
+
     }
 }
