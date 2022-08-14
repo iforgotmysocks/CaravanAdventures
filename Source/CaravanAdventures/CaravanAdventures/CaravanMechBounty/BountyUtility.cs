35aac8cf80b5b813d8a7fe7218130344474c3352
iforgotmysocks <iforgotmysocks@users.noreply.github.com>
2022-08-14 22:16:44 +0200

    added ordering tribals via bounty credits; refactored veteran generation; cleanup

--- Source/CaravanAdventures/CaravanAdventures/CaravanMechBounty/BountyUtility.cs
+++ Source/CaravanAdventures/CaravanAdventures/CaravanMechBounty/BountyUtility.cs
@@ -1,4 +1,5 @@
 ﻿using RimWorld;
+using System;
 using System.Linq;
 using Verse;
 
@@ -6,7 +7,18 @@ namespace CaravanAdventures.CaravanMechBounty
 {
     class BountyUtility
     {
-        public static Pawn GenerateVeteran(TraitDef selPersonality = null, TraitDef selSkill = null)
+        public static Pawn GenerateVeteran(TraitDef selPersonality = null, TraitDef selSkill = null, bool tribal = false)
+        {
+            var veteran = PrepareVeteranPawn(selPersonality, selSkill);
+            if (veteran == null) return null;
+            CheckVeteranChildhoodBackstory(veteran, tribal);
+            CheckVeteranAdultBackstory(veteran);
+            AdjustVeteranSkills(veteran);
+            ConfigureVeteranHediffs(veteran);
+            return veteran;
+        }
+
+        private static Pawn PrepareVeteranPawn(TraitDef selPersonality, TraitDef selSkill)
         {
             var genPawnRequest = new PawnGenerationRequest(CaravanStory.StoryDefOf.CASacrilegHunters_ExperiencedHunter, Faction.OfPlayer)
             {
@@ -26,39 +38,43 @@ namespace CaravanAdventures.CaravanMechBounty
             if (TraitDefOf.Tough != null) veteran.story.traits.GainTrait(new Trait(TraitDefOf.Tough));
             if (selPersonality != null) veteran.story.traits.GainTrait(new Trait(selPersonality, selPersonality.degreeDatas.OrderByDescending(data => data.degree).FirstOrDefault().degree));
             if (selSkill != null) veteran.story.traits.GainTrait(new Trait(selSkill, selSkill.degreeDatas.OrderByDescending(data => data.degree).FirstOrDefault().degree));
+            return veteran;
+        }
 
-            if (veteran.story.childhood.disallowedTraits == null
-                || veteran.story.childhood.disallowedTraits.Any()
-                || veteran.story.childhood.DisabledWorkTypes == null
-                || veteran.story.childhood.DisabledWorkTypes.Any())
+        private static void CheckVeteranChildhoodBackstory(Pawn veteran, bool tribal)
+        {
+            var tribalCatName = MeditationFocusDefOf.Natural?.requiredBackstoriesAny?.FirstOrDefault()?.categoryName ?? "Tribal";
+
+            if ((veteran?.story?.childhood?.disallowedTraits?.Any() ?? true)
+                || (veteran?.story?.childhood?.DisabledWorkTypes?.Any() ?? true
+                || CanUseTribalBackstory(tribal) && (!veteran?.story?.childhood?.spawnCategories?.Contains(tribalCatName) ?? false)))
                 veteran.story.childhood = BackstoryDatabase.allBackstories
                     .Where(backstory =>
-                    (backstory.Value.DisabledWorkTypes == null
-                        || !backstory.Value.DisabledWorkTypes.Any())
-                    && (backstory.Value.disallowedTraits == null
-                        || !backstory.Value.disallowedTraits.Any())
-                    && backstory.Value.slot == BackstorySlot.Childhood
-                    && backstory.Value.skillGainsResolved != null
-                    && (backstory.Value.skillGainsResolved.FirstOrDefault().Key == SkillDefOf.Shooting
-                        || backstory.Value.skillGainsResolved.FirstOrDefault().Key == SkillDefOf.Melee)
-                    && backstory.Value.skillGainsResolved.FirstOrDefault().Value > 0).InRandomOrder().FirstOrDefault().Value;
+                    (CanUseTribalBackstory(tribal) ? backstory.Value.spawnCategories.Contains(tribalCatName) : true)
+                    && (!backstory.Value?.DisabledWorkTypes?.Any() ?? true)
+                    && (!backstory.Value?.disallowedTraits?.Any() ?? true)
+                    && backstory.Value?.slot == BackstorySlot.Childhood
+                    && ((backstory.Value?.skillGainsResolved?.Any(x => x.Key == SkillDefOf.Shooting && x.Value > 0) ?? false)
+                        || (backstory.Value?.skillGainsResolved?.Any(x => x.Key == SkillDefOf.Melee && x.Value > 0) ?? false))
+                    ).InRandomOrder().FirstOrDefault().Value;
+        }
 
-            if (veteran.story.adulthood.disallowedTraits == null
-                || veteran.story.adulthood.disallowedTraits.Any()
-                || veteran.story.adulthood.DisabledWorkTypes == null
-                || veteran.story.adulthood.DisabledWorkTypes.Any())
+        private static void CheckVeteranAdultBackstory(Pawn veteran)
+        {
+            if ((veteran?.story?.adulthood?.disallowedTraits?.Any() ?? true)
+              || (veteran?.story?.adulthood?.DisabledWorkTypes?.Any() ?? true))
                 veteran.story.adulthood = BackstoryDatabase.allBackstories
                     .Where(backstory =>
-                    (backstory.Value.DisabledWorkTypes == null
-                        || !backstory.Value.DisabledWorkTypes.Any())
-                    && (backstory.Value.disallowedTraits == null
-                        || !backstory.Value.disallowedTraits.Any())
+                    (!backstory.Value?.DisabledWorkTypes?.Any() ?? true)
+                    && (!backstory.Value?.disallowedTraits?.Any() ?? true)
                     && backstory.Value.slot == BackstorySlot.Adulthood
-                    && backstory.Value.skillGainsResolved != null
-                    && (backstory.Value.skillGainsResolved.FirstOrDefault().Key == SkillDefOf.Shooting
-                        || backstory.Value.skillGainsResolved.FirstOrDefault().Key == SkillDefOf.Melee)
-                    && backstory.Value.skillGainsResolved.FirstOrDefault().Value > 0).InRandomOrder().FirstOrDefault().Value;
+                    && ((backstory.Value?.skillGainsResolved?.Any(x => x.Key == SkillDefOf.Shooting && x.Value > 0) ?? false)
+                        || (backstory.Value?.skillGainsResolved?.Any(x => x.Key == SkillDefOf.Melee && x.Value > 0) ?? false))
+                    ).InRandomOrder().FirstOrDefault().Value;
+        }
 
+        private static void AdjustVeteranSkills(Pawn veteran)
+        {
             veteran.skills.skills.FirstOrDefault(skill => skill.def == SkillDefOf.Shooting).Level = Rand.Range(15, 19);
             veteran.skills.skills.FirstOrDefault(skill => skill.def == SkillDefOf.Shooting).passion = Rand.Chance(0.8f) ? Passion.Major : Passion.Minor;
 
@@ -70,7 +86,9 @@ namespace CaravanAdventures.CaravanMechBounty
 
             veteran.skills.skills.FirstOrDefault(skill => skill.def == SkillDefOf.Construction).Level = Rand.Range(7, 15);
             veteran.skills.skills.FirstOrDefault(skill => skill.def == SkillDefOf.Construction).passion = Rand.Chance(0.5f) ? Passion.Major : Passion.Minor;
-
+        }
+        private static void ConfigureVeteranHediffs(Pawn veteran)
+        {
             if (ModsConfig.RoyaltyActive)
             {
                 var gland = veteran.health.hediffSet.hediffs.FirstOrDefault(hediff => hediff.def == HediffDef.Named("StoneskinGland"));
@@ -81,8 +99,8 @@ namespace CaravanAdventures.CaravanMechBounty
             if (joywire != null) veteran.health.hediffSet.hediffs.Remove(joywire);
 
             foreach (var hediff in veteran.health.hediffSet.hediffs.Where(hediff => hediff.def.isBad).Reverse()) HealthUtility.Cure(hediff);
-
-            return veteran;
         }
+
+        public static bool CanUseTribalBackstory(bool useTribal) => useTribal && MeditationFocusDefOf.Natural != null && (MeditationFocusDefOf.Natural?.requiredBackstoriesAny?.Any() ?? false);
     }
 }
