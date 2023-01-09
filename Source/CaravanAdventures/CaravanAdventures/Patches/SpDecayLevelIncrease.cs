99666ba1868aab3a029867336c58fb455dcda9e5
iforgotmysocks <iforgotmysocks@users.noreply.github.com>
2023-01-09 12:21:11 +0100

    fixed skill decay level increase not considering passions

--- Source/CaravanAdventures/CaravanAdventures/Patches/SpDecayLevelIncrease.cs
+++ Source/CaravanAdventures/CaravanAdventures/Patches/SpDecayLevelIncrease.cs
@@ -12,6 +12,7 @@ namespace CaravanAdventures.Patches
             var skillOrg = AccessTools.Method(typeof(SkillRecord), nameof(SkillRecord.Learn));
             var skillPost = new HarmonyMethod(typeof(SpDecayLevelIncrease).GetMethod(nameof(Learn_Postfix)));
             HarmonyPatcher.harmony.Patch(skillOrg, null, skillPost);
+
         }
 
         public static void Learn_Postfix(SkillRecord __instance, Pawn ___pawn, float xp, bool direct = false)
@@ -21,7 +22,9 @@ namespace CaravanAdventures.Patches
                 || xp > 0 
                 || (xp < 0f && __instance.levelInt == 0)
                 || __instance.XpRequiredForLevelUp - Math.Abs(xp) <= 0) return;
-            
+
+            xp /= __instance.LearnRateFactor();
+
             if (__instance.levelInt > 15) __instance.Learn(Math.Abs(xp / 2), true);
             else __instance.Learn(Math.Abs(xp), true);
         }
